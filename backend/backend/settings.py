de3c6aed810b104bb19604f67206776bc6dbf017
orvis evans <b.orvis@gmail.com>
2020-12-16 15:18:17 -0800

    Move Secret to environment variable

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import environ
+import os
+
 from pathlib import Path
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "^7=5oq26*!_8tpvtmco*@@8=j17pdp1ze6o_!3pcxe6abf+k0z"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY") or env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
