bab198224e9391524b55ff409fb80c72d93bdd9d
orvis evans <b.orvis@gmail.com>
2020-12-15 23:27:50 -0800

    Purposefully fail a test

--- backend/polls/models.py
+++ backend/polls/models.py
@@ -12,7 +12,7 @@ class Question(models.Model):
 
     def was_published_recently(self):
         now = timezone.now()
-        return now >= self.pub_date >= now - datetime.timedelta(days=1)
+        return self.pub_date >= now - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
