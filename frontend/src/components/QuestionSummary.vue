6e0a8fb8d129d8471e7ec826905aa4c9d69ff4b1
Orvis Evans <b.orvis@gmail.com>
2021-01-03 21:11:48 -0800

    Set up basic poll views and components

---
+++ frontend/src/components/QuestionSummary.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>{{ question.question_text }}</h3>
+    <p class="date">{{ question.pub_date }}</p>
+    <ul>
+      <li v-for="choice in question.choices" :key="choice.id">
+        {{ choice.choice_text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuestionSummary",
+  props: {
+    question: Object,
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>
+      This project is a basic exercise in combining a Django backend with a Vue
+      frontend.
+    </p>
+    <p>
+      The backend and frontend are served from separate servers. The Vue site
+      communicates with the Django backend via a RESTful API.
+    </p>
   </div>
 </template>
